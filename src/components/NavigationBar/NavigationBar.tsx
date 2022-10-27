815fb96fdfc7b78020bf0bd53c58f3e277e502c3
subaru-git <subaru1208.m@gmail.com>
2022-10-27 16:37:59 +0900

    Fix navigationbar link bug Fix NavigationBar n01 link # to n01. Fix change mouse pointer when hober games link.

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -140,25 +140,31 @@ const DesktopSubNav = ({ label, href, subLabel }: NavItem) => {
       _hover={{ bg: useColorModeValue('pink.50', 'gray.900') }}
     >
       <Link href={href ?? '/'}>
-        <Stack direction={'row'} align={'center'}>
-          <Box>
-            <Text transition={'all .3s ease'} _groupHover={{ color: 'pink.400' }} fontWeight={500}>
-              {label}
-            </Text>
-            <Text fontSize={'sm'}>{subLabel}</Text>
-          </Box>
-          <Flex
-            transition={'all .3s ease'}
-            transform={'translateX(-10px)'}
-            opacity={0}
-            _groupHover={{ opacity: '100%', transform: 'translateX(0)' }}
-            justify={'flex-end'}
-            align={'center'}
-            flex={1}
-          >
-            <Icon color={'pink.400'} w={5} h={5} as={GoChevronRight} />
-          </Flex>
-        </Stack>
+        <a>
+          <Stack direction={'row'} align={'center'}>
+            <Box>
+              <Text
+                transition={'all .3s ease'}
+                _groupHover={{ color: 'pink.400' }}
+                fontWeight={500}
+              >
+                {label}
+              </Text>
+              <Text fontSize={'sm'}>{subLabel}</Text>
+            </Box>
+            <Flex
+              transition={'all .3s ease'}
+              transform={'translateX(-10px)'}
+              opacity={0}
+              _groupHover={{ opacity: '100%', transform: 'translateX(0)' }}
+              justify={'flex-end'}
+              align={'center'}
+              flex={1}
+            >
+              <Icon color={'pink.400'} w={5} h={5} as={GoChevronRight} />
+            </Flex>
+          </Stack>
+        </a>
       </Link>
     </Box>
   )
@@ -241,7 +247,7 @@ const GetNavItem = () => {
         {
           label: '501',
           subLabel: t.N01_DESCRIPTION,
-          href: '#',
+          href: '/n01',
         },
         {
           label: "Eagle's Eye",
