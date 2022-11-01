cedc292a731a565c00a8a1e5a4da13f399de7a2e
subaru-git <subaru1208.m@gmail.com>
2022-11-01 22:46:58 +0900

    ADD BULL Button animation ADD BULL Button animation

--- src/components/CountBullButtons/CountBullButtons.tsx
+++ src/components/CountBullButtons/CountBullButtons.tsx
@@ -10,19 +10,9 @@ const CountBullButtons: FC<CountBullButtonsProps> = ({ onCount, disabled = false
   return (
     <div css={styles}>
       <div css={[disableStyle, disabled ? null : css({ display: 'none' })]} />
-      <input
-        css={outerBullStyles}
-        type='button'
-        onClick={() => onCount('S-BULL')}
-        disabled={disabled}
-      />
-      <input
-        css={inBullStyles}
-        type='button'
-        onClick={() => onCount('D-BULL')}
-        disabled={disabled}
-      />
-      <input css={nonBullStyles} type='button' onClick={() => onCount('0')} disabled={disabled} />
+      <button css={outerBullStyles} onClick={() => onCount('S-BULL')} disabled={disabled} />
+      <button css={inBullStyles} onClick={() => onCount('D-BULL')} disabled={disabled} />
+      <button css={nonBullStyles} onClick={() => onCount('0')} disabled={disabled} />
     </div>
   )
 }
@@ -44,9 +34,37 @@ const inBullStyles = css({
   maxWidth: '15vh',
   maxHeight: '15vh',
   color: 'white',
-  '-webkit-appearance': 'none',
   backgroundImage: 'radial-gradient(circle, #111111 0% 40%, #666666 90% 100%)',
   zIndex: 3,
+  WebkitAppearance: 'none',
+  '&:hover': {
+    backgroundImage: 'radial-gradient(circle, #111111 0% 40%, #444444 90% 100%)',
+  },
+  '&:active::after': {
+    content: '""',
+    position: 'absolute',
+    borderRadius: '100%',
+    height: '15vw',
+    width: '15vw',
+    maxWidth: '15vh',
+    maxHeight: '15vh',
+    marginTop: '-50%',
+    marginLeft: '-50%',
+    verticalAlign: 'text-top',
+    background: '#777777',
+    opacity: 0,
+    animation: 'ripple 0.3s ease-out',
+  },
+  '@keyframes ripple': {
+    '0%': {
+      opacity: 0.8,
+      transform: 'scale(1.3)',
+    },
+    '100%': {
+      opacity: 0,
+      transform: 'scale(0.0)',
+    },
+  },
 })
 const outerBullStyles = css({
   position: 'absolute',
@@ -55,20 +73,50 @@ const outerBullStyles = css({
   width: '30vw',
   maxWidth: '30vh',
   maxHeight: '30vh',
-  color: 'white',
   verticalAlign: 'text-top',
   backgroundImage: 'radial-gradient(circle, #ff2222 0% 60%, #ff6666 90% 100%)',
-  '-webkit-appearance': 'none',
+  WebkitAppearance: 'none',
   zIndex: 2,
+  '&:hover': {
+    backgroundImage: 'radial-gradient(circle, #ff0000 0% 60%, #ff4444 90% 100%)',
+  },
+  '&:active::after': {
+    content: '""',
+    position: 'absolute',
+    borderRadius: '100%',
+    height: '30vw',
+    width: '30vw',
+    maxWidth: '30vh',
+    maxHeight: '30vh',
+    marginTop: '-50%',
+    marginLeft: '-50%',
+    verticalAlign: 'text-top',
+    background: '#dd7777',
+    opacity: 0,
+    animation: 'ripple 0.3s ease-out',
+  },
+  '@keyframes ripple': {
+    '0%': {
+      opacity: 0.8,
+      transform: 'scale(0.6)',
+    },
+    '100%': {
+      opacity: 0,
+      transform: 'scale(1.3)',
+    },
+  },
 })
 const nonBullStyles = css({
   height: '40vw',
   width: '40vw',
-  '-webkit-appearance': 'none',
+  WebkitAppearance: 'none',
   backgroundImage: 'radial-gradient(circle, #777777 0% 70%, #666666 90% 100%)',
   zIndex: 1,
   maxWidth: '40vh',
   maxHeight: '40vh',
+  '&:hover': {
+    backgroundImage: 'radial-gradient(circle, #666666 0% 70%, #555555 90% 100%)',
+  },
 })
 const disableStyle = css({
   backgroundColor: '#222222',
