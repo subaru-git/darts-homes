c3f657320dce54983870bac08ec916dfe53356a3
subaru-git <subaru1208.m@gmail.com>
2022-11-15 10:49:52 +0900

    Add Dart Board Add Dart Board

---
+++ src/components/DartBoard/DartBoard.stories.tsx
@@ -0,0 +1,18 @@
+/* eslint-disable import/named */
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import DartBoard from './DartBoard';
+
+export default {
+  title: 'Components/DartBoard',
+  component: DartBoard,
+} as ComponentMeta<typeof DartBoard>;
+
+const Template: ComponentStory<typeof DartBoard> = (args) => (
+  <div css={{ width: '640px', height: '640px' }}>
+    <DartBoard {...args} />
+  </div>
+);
+
+export const Default = Template.bind({});
+Default.args = {};
