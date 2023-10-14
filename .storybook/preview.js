2fb07d9879ff4c9a63d12393e96452478f16f2a1
subaru-git <subaru1208.m@gmail.com>
2023-10-14 12:23:39 +0900

    update next

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,4 @@
-import { RouterContext } from 'next/dist/shared/lib/router-context';
+import { RouterContext } from 'next/dist/shared/lib/router-context.shared-runtime';
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 import '../src/styles/globals.css';
 
