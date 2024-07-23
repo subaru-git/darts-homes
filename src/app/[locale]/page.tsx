143b7d6800f1b86f11934c176362e29882bbe6f5
subaru-git <subaru1208.m@gmail.com>
2024-07-23 13:20:46 +0900

    ogp test

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,4 +1,21 @@
 import Main from '@/containers/Home/Main';
+import { url } from 'inspector';
+import { getTranslations } from 'next-intl/server';
+
+export const generateMetadata = async ({ params: { locale } }: { params: { locale: string } }) => {
+  const t = await getTranslations({ locale, namespace: 'seo.default' });
+  return {
+    title: t('title'),
+    description: t('description'),
+    openGraph: {
+      title: t('title'),
+      description: t('description'),
+      type: 'website',
+      images: [{ url: 'https://darts.homes/ogp.png', width: 800, height: 600, alt: 'Darts Homes' }],
+    },
+    canonical: 'https://darts.homes',
+  };
+};
 
 const Home = () => {
   return <Main />;
